32494445703e4ed93b1e0463513ad1bade610975
TCotton <me@andywalpole.me>
2025-12-03 14:58:42 +0000

    chore: added support for react compiler to astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -3,7 +3,13 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react({
+    babel: {
+      plugins: [
+        ['babel-plugin-react-compiler']
+      ]
+    }
+  })],
   output: 'static',
   vite: {
     test: {
