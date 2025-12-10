a3a57284bf218c88086295f52a4dab0f769739ef
TCotton <me@andywalpole.me>
2025-12-10 13:59:45 +0000

    feat: added FastifyUtilsUtil class

---
+++ apps/backend/src/shared/utils/fastily.utils.ts
@@ -0,0 +1,16 @@
+import type { FastifyRequest} from 'fastify'
+
+export class FastifyUtilsUtil {
+
+  static convertFastifyRequest(request: FastifyRequest): { headers: Record<string, string> } {
+    const headers: Record<string, string> = {}
+    Object.entries(request.headers).forEach(([key, value]) => {
+      if (typeof value === 'string') {
+        headers[key] = value
+      } else if (Array.isArray(value)) {
+        headers[key] = value.join(', ')
+      }
+    })
+    return { headers }
+  }
+}
